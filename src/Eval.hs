a32df3e45977a441b4b62d64b696af308cf84c32
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-09-07 21:40:08 -0500

    Add implementation stubs to Eval.hs

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,3 +1,23 @@
 module Eval where
 
 eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ func `lookup` primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+-- TODO: Implement.
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = undefined
+
+-- TODO: Implement.
+unpackNum :: LispVal -> Integer
+unpackNum = undefined
